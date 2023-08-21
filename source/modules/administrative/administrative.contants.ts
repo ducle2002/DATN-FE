cad13b49a8fd5488c1b44852143f9bcb0a141cfd
kienndhn <nguyendinhkien12344321@gmail.com>
2023-08-21 14:06:49 +0700

    fix: crash administrative

--- source/modules/administrative/administrative.contants.ts
+++ source/modules/administrative/administrative.contants.ts
@@ -2,16 +2,16 @@ import language, {languageKeys} from '@/config/language/language';
 
 export const FILTER_FORMID = [
   {
-    name: language.t(languageKeys.administrative.language.requesting),
+    name: language.t(languageKeys.administrative.main.requesting),
     type: 21,
   },
   {
-    name: language.t(languageKeys.administrative.language.accept),
+    name: language.t(languageKeys.administrative.main.accept),
 
     type: 22,
   },
   {
-    name: language.t(languageKeys.administrative.language.decline),
+    name: language.t(languageKeys.administrative.main.decline),
     type: 23,
   },
 ];
