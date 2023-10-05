0ecd7eb9d45aebc40f913a591aa5b590e12195b0
CVHOANG-coder <mrgreencat99@gmail.com>
2023-10-05 14:44:23 +0700

    chore

--- source/routes/work-management.stack.tsx
+++ source/routes/work-management.stack.tsx
@@ -1,3 +1,4 @@
+import AddNewWorkScreen from '@/screens/work-management/add-new-work.screen';
 import AssignmentScreen from '@/screens/work-management/assignment.screen';
 import ManagementScreen from '@/screens/work-management/management.screen';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -6,6 +7,7 @@ import React from 'react';
 export type WorkManagementStackParamsList = {
   MANAGEMENT: undefined;
   ASSIGNMENT: undefined;
+  NEW_WORK: undefined;
 };
 const Stack = createStackNavigator<WorkManagementStackParamsList>();
 
@@ -18,6 +20,7 @@ const WorkManagementStack = () => {
       }}>
       <Stack.Screen name="MANAGEMENT" component={ManagementScreen} />
       <Stack.Screen name="ASSIGNMENT" component={AssignmentScreen} />
+      <Stack.Screen name="NEW_WORK" component={AddNewWorkScreen} />
     </Stack.Navigator>
   );
 };
---
+++ source/screens/work-management/add-new-work.screen.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import language, {languageKeys} from '@/config/language/language';
+
+const AddNewWorkScreen = () => {
+  return (
+    <View>
+      {/* <Text>{language.t(languageKeys.)}</Text> */}
+      <Text>Tạo công việc mới</Text>
+    </View>
+  );
+};
+
+export default AddNewWorkScreen;
+
+const styles = StyleSheet.create({});
